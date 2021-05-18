a8952a11d97a73e3383cb252caad42d7a689651b
tama-0 <67673521+tama-0@users.noreply.github.com>
2021-05-18 23:10:19 +0900

    Add files via upload

---
+++ OwnMap/OwnMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OwnMap
+//
+//  Created by 玉川悠真 on 2021/05/18.
+//  Copyright © 2021年 玉川悠真. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
