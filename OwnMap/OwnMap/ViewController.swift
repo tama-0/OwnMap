314e9da153128cb8eb3e2bf497ba61e3f369ae2a
Tama <tamagawayuuma@icloud.com>
2021-05-18 23:26:00 +0900

    mapview

--- OwnMap/OwnMap/ViewController.swift
+++ OwnMap/OwnMap/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
